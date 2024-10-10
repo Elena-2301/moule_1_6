5e024c61493de912930e4c642301cdbb30cf372e
Elena-2301 <elena.pehnova@gmail.com>
2024-10-10 06:56:49 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,11 @@
+number_of_completed_lessons = 12
+print(number_of_completed_lessons)
+number_of_hours_spent = 1.5
+print(number_of_hours_spent)
+course_name = 'Python'
+print(course_name)
+print(number_of_hours_spent / number_of_completed_lessons)
+print(course_name, number_of_completed_lessons, number_of_hours_spent, (number_of_hours_spent / number_of_completed_lessons))
+
+
+
